8914ac9f6fd13cf1e08e2adca105aed8334c123b
Diana Ramírez <diacrami@espol.edu.ec>
2025-09-01 15:10:57 -0500

    Update Headerbar.jsx

--- src/ui/components/Headerbar.jsx
+++ src/ui/components/Headerbar.jsx
@@ -2,7 +2,7 @@
 export const Headerbar = () => {
   return (
     <div className="h-[3rem] flex items-center p-2 bg-slate-700 text-slate-200">
-        <h2>Trello Clone</h2>
+        <h2>Trello Clone Feature z</h2>
     </div>
   )
 }
